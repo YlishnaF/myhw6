57811837d650dea9a95bc49f888c0ce8029af7e8
geetbrains <ylishnaf@yandex.ru>
2020-04-26 18:21:46 +0500

    дз6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Cat cat = new Cat(150,0,1);
+        Cat cat1 = new Cat(250,0,1.5f);
+        Dog dog = new Dog(500,10,2);
+
+        cat.isJump(2);
+        cat1.isRun(100);
+        System.out.println(Animal.animalCount);
+        System.out.println(Cat.catCount);
+
 
     }
 }
